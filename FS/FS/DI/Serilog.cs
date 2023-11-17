bde95200d58c8571b32e37b5af624255921403be
VadimZharikov <vadimzharikov2003@gmail.com>
2023-11-17 17:22:33 +0300

    add Serilog logger

---
+++ FS/FS/DI/Serilog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace FS.WebAPI.DI
+{
+    public static class Serilog
+    {
+        public static IServiceCollection AddSerilogServices(
+        this IServiceCollection services,
+        LoggerConfiguration configuration)
+        {
+            Log.Logger = configuration.CreateLogger();
+            AppDomain.CurrentDomain.ProcessExit += (s, e) => Log.CloseAndFlush();
+            return services.AddSingleton(Log.Logger);
+        }
+    }
+}
