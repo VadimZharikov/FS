bc3e7aff555c7f831dec7e695224a2411177e436
VadimZharikov <vadimzharikov2003@gmail.com>
2023-11-18 15:28:16 +0300

    register dbcontext and repositories, add conn string

---
+++ FS/FS.BLL/DI/BusinessLogicLayerReg.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FS.BLL.DI
+{
+    public static class BusinessLogicLayerReg
+    {
+        public static void AddBusinessLogic(this IServiceCollection services, IConfiguration configuration)
+        {
+            DAL.DI.DataLayerReg.AddDataRepository(services, configuration);
+        }
+    }
+}
