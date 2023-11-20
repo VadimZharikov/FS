5e1eb625e9357b881afb2089384e101fa43f0407
VadimZharikov <vadimzharikov2003@gmail.com>
2023-11-20 16:05:20 +0300

    DAL untegration tests

---
+++ FS/FS.DAL.UnitTests/IntegrationTests/Repositories/Actor/ActorRepositoryTests.cs
@@ -0,0 +1,210 @@
+﻿using FS.DAL.DataContext;
+using FS.DAL.Entities;
+using FS.DAL.Repositories;
+
+namespace FS.DAL.UnitTests.IntegrationTests.Repositories.Actor
+{
+    public class ActorRepositoryTests
+    {
+        private readonly DbContextOptions<DatabaseContext> _options;
+        private ActorRepository _repository;
+
+        public ActorRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "FSDatabase" + DateTime.Now.ToFileTimeUtc())
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetActor_ValidId_ReturnsActorEntity()
+        {
+            var model = new ActorEntity
+            {
+                ActorName = "asd",
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Actors.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var actor = await _repository.GetActor(entity.Entity.ActorId);
+
+            actor.ShouldNotBeNull();
+            actor.ShouldBeEquivalentTo(entity.Entity);
+        }
+
+        [Fact]
+        public async Task ActorExists_ValidId_ReturnsTrue()
+        {
+            var model = new ActorEntity
+            {
+                ActorName = "asd"                
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Actors.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var actor = await _repository.ActorExists(entity.Entity.ActorId);
+
+            actor.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task GetActors_ValidModels_ReturnsActorEntityList()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            context.Actors.Add(new ActorEntity
+            {
+                ActorName = "asd"
+            });
+
+            context.Actors.Add(new ActorEntity
+            {
+                ActorName = "dsa"
+            });
+
+            await context.SaveChangesAsync();
+
+            var actor = await _repository.GetActors();
+
+            actor.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task AddActor_ValidModel_ReturnsActorEntity()
+        {
+            var model = new ActorEntity()
+            {
+                ActorName = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var actor = await _repository.AddActor(model);
+
+            actor.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task UpdateActor_ValidModel_ReturnsActorEntity()
+        {
+            var addedModel = new ActorEntity
+            {
+                ActorName = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Actors.AddAsync(addedModel);
+            await context.SaveChangesAsync();
+
+            var actor = await _repository.UpdateActor(entity.Entity);
+
+            actor.ShouldNotBeNull();
+            actor.ShouldBeEquivalentTo(entity.Entity);
+        }
+
+        [Fact]
+        public async Task DeleteActor_ValidId_NotThrow()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            context.Actors.Add(new ActorEntity
+            {
+                ActorName = "asd"
+            });
+            await context.SaveChangesAsync();
+
+            await _repository.DeleteActor(1).ShouldNotThrowAsync();
+        }
+
+        [Fact]
+        public async Task GetActor_InvalidId_ThrowsKeyNotFoundException()
+        {
+            var model = new ActorEntity
+            {
+                ActorName = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            context.Actors.Add(model);
+            await context.SaveChangesAsync();
+
+            await _repository.GetActor(-8).ShouldThrowAsync(typeof(KeyNotFoundException));
+        }
+
+        [Fact]
+        public async Task ActorExists_InvalidId_ReturnsFalse()
+        {
+            var model = new ActorEntity
+            {
+                ActorName = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Actors.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var actor = await _repository.ActorExists(entity.Entity.ActorId-1);
+
+            actor.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task GetActors_NoModels_ReturnsEmptyActorEntityList()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await context.Database.EnsureDeletedAsync();
+            var actor = await _repository.GetActors();
+
+            actor.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public async Task AddActor_InvalidModel_ThrowArgumentNullException()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await _repository.AddActor(null).ShouldThrowAsync(typeof(ArgumentNullException));
+        }
+
+        [Fact]
+        public async Task UpdateActor_InvalidModel_ThrowDbUpdateConcurrencyException()
+        {
+            var expectedModel = new ActorEntity
+            {
+                ActorName = "Name"
+            };
+
+            var addedModel = new ActorEntity();
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await context.Actors.AddAsync(addedModel);
+            await context.SaveChangesAsync();
+
+            await _repository.UpdateActor(expectedModel).ShouldThrowAsync(typeof(DbUpdateConcurrencyException));
+        }
+
+        [Fact]
+        public async Task DeleteActor_InvalidId_ThrowKeyNotFoundException()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            await _repository.DeleteActor(-123).ShouldThrowAsync(typeof(KeyNotFoundException));
+        }
+    }
+}
---
+++ FS/FS.DAL.UnitTests/IntegrationTests/Repositories/Film/FilmRepositoryTests.cs
@@ -0,0 +1,251 @@
+﻿using FS.DAL.DataContext;
+using FS.DAL.Entities;
+using FS.DAL.Repositories;
+
+namespace FS.DAL.UnitTests.IntegrationTests.Repositories.Film
+{
+    public class FilmRepositoryTests
+    {
+        private readonly DbContextOptions<DatabaseContext> _options;
+        private FilmRepository _repository;
+
+        public FilmRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "FSDatabase" + DateTime.Now.ToFileTimeUtc())
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetFilm_ValidId_ReturnsFilmEntity()
+        {
+            var model = new FilmEntity
+            {
+                Title = "asd",
+                Actors = new List<ActorEntity>
+                {
+                    new()
+                    {
+                        ActorName = "asd",
+                    }
+                },
+                Reviews = new List<ReviewEntity>
+                {
+                    new()
+                    {
+                        Title = "asd"
+                    }
+                }
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Films.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var film = await _repository.GetFilm(entity.Entity.FilmId);
+
+            film.ShouldNotBeNull();
+            film.ShouldBeEquivalentTo(entity.Entity);
+        }
+
+        [Fact]
+        public async Task FilmExists_ValidId_ReturnsTrue()
+        {
+            var model = new FilmEntity
+            {
+                Title = "asd",
+                Actors = new List<ActorEntity>
+                {
+                    new()
+                    {
+                        ActorName = "asd",
+                    }
+                },
+                Reviews = new List<ReviewEntity>
+                {
+                    new()
+                    {
+                        Title = "asd"
+                    }
+                }
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Films.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var film = await _repository.FilmExists(entity.Entity.FilmId);
+
+            film.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task GetFilms_ValidModels_ReturnsFilmEntityList()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            context.Films.Add(new FilmEntity
+            {
+                Title = "asd"
+            });
+
+            context.Films.Add(new FilmEntity
+            {
+                Title = "dsa"
+            });
+
+            await context.SaveChangesAsync();
+
+            var film = await _repository.GetFilms();
+
+            film.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task AddFilm_ValidModel_ReturnsFilmEntity()
+        {
+            var model = new FilmEntity()
+            {
+                Title = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var film = await _repository.AddFilm(model);
+
+            film.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task UpdateFilm_ValidModel_ReturnsFilmEntity()
+        {
+            var addedModel = new FilmEntity
+            {
+                Title = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Films.AddAsync(addedModel);
+            await context.SaveChangesAsync();
+
+            var film = await _repository.UpdateFilm(entity.Entity);
+
+            film.ShouldNotBeNull();
+            film.ShouldBeEquivalentTo(entity.Entity);
+        }
+
+        [Fact]
+        public async Task DeleteFilm_ValidId_NotThrow()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            context.Films.Add(new FilmEntity
+            {
+                Title = "asd"
+            });
+            await context.SaveChangesAsync();
+
+            await _repository.DeleteFilm(1).ShouldNotThrowAsync();
+        }
+
+        [Fact]
+        public async Task GetFilm_InvalidId_ThrowsKeyNotFoundException()
+        {
+            var model = new FilmEntity
+            {
+                Title = "asd"
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            context.Films.Add(model);
+            await context.SaveChangesAsync();
+
+            await _repository.GetFilm(-8).ShouldThrowAsync(typeof(KeyNotFoundException));
+        }
+
+        [Fact]
+        public async Task FilmExists_InvalidId_ReturnsFalse()
+        {
+            var model = new FilmEntity
+            {
+                Title = "asd",
+                Actors = new List<ActorEntity>
+                {
+                    new()
+                    {
+                        ActorName = "asd",
+                    }
+                },
+                Reviews = new List<ReviewEntity>
+                {
+                    new()
+                    {
+                        Title = "asd"
+                    }
+                }
+            };
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            var entity = await context.Films.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            var film = await _repository.FilmExists(entity.Entity.FilmId-1);
+
+            film.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task GetFilms_NoModels_ReturnsEmptyFilmEntityList()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await context.Database.EnsureDeletedAsync();
+            var film = await _repository.GetFilms();
+
+            film.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public async Task AddFilm_InvalidModel_ThrowArgumentNullException()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await _repository.AddFilm(null).ShouldThrowAsync(typeof(ArgumentNullException));
+        }
+
+        [Fact]
+        public async Task UpdateFilm_InvalidModel_ThrowDbUpdateConcurrencyException()
+        {
+            var expectedModel = new FilmEntity
+            {
+                Title = "Name"
+            };
+
+            var addedModel = new FilmEntity();
+
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+
+            await context.Films.AddAsync(addedModel);
+            await context.SaveChangesAsync();
+
+            await _repository.UpdateFilm(expectedModel).ShouldThrowAsync(typeof(DbUpdateConcurrencyException));
+        }
+
+        [Fact]
+        public async Task DeleteFilm_InvalidId_ThrowKeyNotFoundException()
+        {
+            await using DatabaseContext context = new(_options);
+            _repository = new(context);
+            await _repository.DeleteFilm(-123).ShouldThrowAsync(typeof(KeyNotFoundException));
+        }
+    }
+}
