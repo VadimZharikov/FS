93bf6ddad00fd7b9326f4119c82bdb11743b69c5
VadimZharikov <vadimzharikov2003@gmail.com>
2023-11-20 18:58:00 +0300

    Business logic tests

---
+++ FS/FS.BLL.UnitTests/Services/Films/FilmServiceTests.cs
@@ -0,0 +1,170 @@
+﻿using AutoMapper;
+using FS.BLL.Entities;
+using FS.BLL.Services;
+using FS.BLL.Utilities;
+using FS.DAL.Entities;
+using FS.DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace FS.BLL.UnitTests.Services.Films
+{
+    public class FilmServiceTests
+    {
+        private readonly Mock<IFilmRepository> _repoMock = new Mock<IFilmRepository>();
+        private readonly Mock<ILogger<FilmService>> _loggerMock = new Mock<ILogger<FilmService>>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+        private readonly FilmService _service;
+
+        public FilmServiceTests()
+        {
+            _service = new FilmService(_repoMock.Object, _mapperMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task GetFilm_ValidId_ReturnsFilmById()
+        {
+            var filmId = new Random().Next();
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            var filmTest = new Film { FilmId = filmId, Title = filmName, Stars = 5.0F };
+
+            _repoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<FilmEntity, Film>(filmTestEntity))
+                .Returns(filmTest);
+
+            var film = await _service.GetFilm(filmId);
+
+            _repoMock.Verify(x => x.GetFilm(filmId));
+            filmId.ShouldBe(film.FilmId);
+            filmName.ShouldBe(film.Title);
+        }
+        [Fact]
+        public async Task GetFilms_ReturnsFilmsList()
+        {
+            _repoMock.Setup(x => x.GetFilms()).ReturnsAsync(It.IsAny<List<FilmEntity>>());
+            _mapperMock.Setup(x => x.Map<List<FilmEntity>, List<Film>>(It.IsAny<List<FilmEntity>>()))
+                .Returns(new List<Film>());
+
+            var films = await _service.GetFilms();
+
+            _repoMock.Verify(x => x.GetFilms());
+            films.ShouldBeOfType<List<Film>>();
+        }
+        [Fact]
+        public async Task AddFilm_ValidFilm_ReturnsTrue()
+        {
+            var filmId = new Random().Next();
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            var filmTest = new Film { FilmId = filmId, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.AddFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Film, FilmEntity>(filmTest))
+                .Returns(filmTestEntity);
+
+            var film = await _service.AddFilm(filmTest);
+
+            _repoMock.Verify(x => x.AddFilm(filmTestEntity));
+            film.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task AddFilm_InValidFilm_ReturnsFalse()
+        {
+            var filmId = 0;
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            var filmTest = new Film { FilmId = filmId, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.AddFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Film, FilmEntity>(filmTest))
+                .Returns(filmTestEntity);
+
+            var film = await _service.AddFilm(filmTest);
+
+            _repoMock.Verify(x => x.AddFilm(filmTestEntity));
+            film.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task PutFilm_ValidFilm_ReturnsTrue()
+        {
+            var filmId = new Random().Next();
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var reviews_1 = new List<ReviewEntity>() { new() { FilmId = filmId } };
+            var reviews_2 = new List<Review>() { new() { FilmId = filmId } };
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars, Reviews = reviews_1 };
+            var filmTest = new Film { FilmId = filmId, Title = filmName, Stars = stars, Reviews = reviews_2 };
+            _repoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _repoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Film, FilmEntity>(filmTest))
+                .Returns(filmTestEntity);
+
+            var film = await _service.PutFilm(filmId, filmTest);
+
+            _repoMock.Verify(x => x.UpdateFilm(filmTestEntity));
+            film.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task PutFilm_InValidFilm_ReturnsFalse()
+        {
+            var filmId = 0;
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            var filmTest = new Film { FilmId = filmId, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Film, FilmEntity>(filmTest))
+                .Returns(filmTestEntity);
+
+            var film = await _service.PutFilm(filmId, filmTest);
+
+            film.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task PutFilm_DifferentIdFilm_ReturnsFalse()
+        {
+            var filmId = 1;
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            var filmTest = new Film { FilmId = filmId + 1, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Film, FilmEntity>(filmTest))
+                .Returns(filmTestEntity);
+
+            var film = await _service.PutFilm(filmId, filmTest);
+
+            film.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task DeleteFilm_ValidFilm_ReturnsTrue()
+        {
+            var filmId = new Random().Next();
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.DeleteFilm(filmId)).ReturnsAsync(filmTestEntity);
+
+            var film = await _service.DeleteFilm(filmId);
+
+            _repoMock.Verify(x => x.DeleteFilm(filmId));
+            film.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task DeleteFilm_InValidFilm_ReturnsFalse()
+        {
+            var filmId = 0;
+            var filmName = "Ok";
+            var stars = 5.0F;
+            var filmTestEntity = new FilmEntity { FilmId = filmId, Title = filmName, Stars = stars };
+            _repoMock.Setup(x => x.DeleteFilm(filmId)).ReturnsAsync(filmTestEntity);
+
+            var film = await _service.DeleteFilm(filmId);
+
+            _repoMock.Verify(x => x.DeleteFilm(filmId));
+            film.ShouldBeFalse();
+        }
+    }
+}
---
+++ FS/FS.BLL.UnitTests/Services/Reviews/ReviewServiceTests.cs
@@ -0,0 +1,336 @@
+﻿using AutoMapper;
+using FS.BLL.Entities;
+using FS.BLL.Services;
+using FS.DAL.Entities;
+using FS.DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace FS.BLL.UnitTests.Services.Reviews
+{
+    public class ReviewServiceTests
+    {
+        private readonly Mock<IReviewRepository> _reviewRepoMock = new Mock<IReviewRepository>();
+        private readonly Mock<IFilmRepository> _filmRepoMock = new Mock<IFilmRepository>();
+        private readonly Mock<ILogger<ReviewService>> _loggerMock = new Mock<ILogger<ReviewService>>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+        private readonly ReviewService _service;
+
+        public ReviewServiceTests()
+        {
+            _service = new ReviewService(_reviewRepoMock.Object, _filmRepoMock.Object, _mapperMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task GetReview_ValidId_ReturnsReviewById()
+        {
+            var reviewId = new Random().Next();
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity { 
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId 
+            };
+            var reviewTest = new Review { 
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+
+            _reviewRepoMock.Setup(x => x.GetReview(reviewId)).ReturnsAsync(reviewTestEntity);
+            _mapperMock.Setup(x => x.Map<ReviewEntity, Review>(reviewTestEntity))
+                .Returns(reviewTest);
+
+            var review = await _service.GetReview(reviewId);
+
+            _reviewRepoMock.Verify(x => x.GetReview(reviewId));
+            reviewId.ShouldBe(review.ReviewId);
+            reviewName.ShouldBe(review.Title);
+            stars.ShouldBe(review.Stars);
+        }
+        [Fact]
+        public async Task GetReviews_ReturnsReviewsList()
+        {
+            _reviewRepoMock.Setup(x => x.GetReviews()).ReturnsAsync(It.IsAny<List<ReviewEntity>>());
+            _mapperMock.Setup(x => x.Map<List<ReviewEntity>, List<Review>>(It.IsAny<List<ReviewEntity>>()))
+                .Returns(new List<Review>());
+
+            var reviews = await _service.GetReviews();
+
+            _reviewRepoMock.Verify(x => x.GetReviews());
+            reviews.ShouldBeOfType<List<Review>>();
+        }
+        [Fact]
+        public async Task AddReview_ValidReview_ReturnsTrue()
+        {
+            var reviewId = new Random().Next();
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity};
+
+            _reviewRepoMock.Setup(x => x.AddReview(reviewTestEntity)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.AddReview(reviewTest);
+
+            _reviewRepoMock.Verify(x => x.AddReview(reviewTestEntity));
+            review.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task AddReview_InValidReview_ReturnsFalse()
+        {
+            var reviewId = 0;
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.AddReview(reviewTestEntity)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.AddReview(reviewTest);
+
+            _reviewRepoMock.Verify(x => x.AddReview(reviewTestEntity));
+            review.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task PutReview_ValidReview_ReturnsTrue()
+        {
+            var reviewId = new Random().Next();
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.UpdateReview(reviewTestEntity)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.PutReview(reviewId, reviewTest);
+
+            _reviewRepoMock.Verify(x => x.UpdateReview(reviewTestEntity));
+            review.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task PutReview_InValidReview_ReturnsFalse()
+        {
+            var reviewId = 0;
+            var filmId = new Random().Next();
+            var reviewName = "";
+            var stars = -1;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.UpdateReview(reviewTestEntity)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.PutReview(reviewId, reviewTest);
+
+            review.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task PutReview_DifferentIdReview_ReturnsFalse()
+        {
+            var reviewId = new Random().Next();
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.UpdateReview(reviewTestEntity)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.PutReview(0, reviewTest);
+
+            review.ShouldBeFalse();
+        }
+        [Fact]
+        public async Task DeleteReview_ValidId_ReturnsTrue()
+        {
+            var reviewId = new Random().Next();
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.DeleteReview(reviewId)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.DeleteReview(reviewId);
+
+            _reviewRepoMock.Verify(x => x.DeleteReview(reviewId));
+            review.ShouldBeTrue();
+        }
+        [Fact]
+        public async Task DeleteReview_InValidId_ReturnsFalse()
+        {
+            var reviewId = 0;
+            var filmId = new Random().Next();
+            var reviewName = "Ok";
+            var stars = 5;
+            var filmTest = new Film { FilmId = filmId };
+            var filmTestEntity = new FilmEntity { FilmId = filmId };
+            var reviewTestEntity = new ReviewEntity
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTestEntity,
+                FilmId = filmId
+            };
+            var reviewTest = new Review
+            {
+                ReviewId = reviewId,
+                Title = reviewName,
+                Stars = stars,
+                Film = filmTest,
+                FilmId = filmId
+            };
+            filmTestEntity.Reviews = new List<ReviewEntity>() { reviewTestEntity };
+
+            _reviewRepoMock.Setup(x => x.DeleteReview(reviewId)).ReturnsAsync(reviewTestEntity);
+            _filmRepoMock.Setup(x => x.UpdateFilm(filmTestEntity)).ReturnsAsync(filmTestEntity);
+            _filmRepoMock.Setup(x => x.GetFilm(filmId)).ReturnsAsync(filmTestEntity);
+            _mapperMock.Setup(x => x.Map<Review, ReviewEntity>(reviewTest))
+                .Returns(reviewTestEntity);
+
+            var review = await _service.DeleteReview(reviewId);
+
+            _reviewRepoMock.Verify(x => x.DeleteReview(reviewId));
+            review.ShouldBeFalse();
+        }
+    }
+}
